9dbac1447b46e556341e9b72ff9abc82f684cb8a
Nielshen <Niels.hensler@gmail.com>
2024-09-18 13:16:52 +0200

    github pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // benutzerdefinierte Domain verwenden
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
