b2b27a979b1542302680a1c362b6246d4148c34c
Nielshen <Niels.hensler@gmail.com>
2024-09-18 19:46:15 +0200

    Improvements and pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import { Button } from "./components/ui/button"
 import ImageGallery from './components/ImageGallery';
--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ChevronLeft, ChevronRight, School, GraduationCap } from 'lucide-react';
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
