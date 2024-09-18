04a9db82e92c636cbf15581f7e37ea17ce3d3a3f
Nielshen <Niels.hensler@gmail.com>
2024-09-18 11:07:27 +0200

    react webpage pipeline fix

--- betterclassroom/vite.config.ts
+++ betterclassroom/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BetterClassroomLanding/',
+  base: '/betterclassroom/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
